6c8241a14a7985b942ad98b949bcf754fc36354b
indigoandlavender <happy@indigoandlavender.love>
2026-01-23 10:22:47 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://festivalsinmorocco.com',
   output: 'static',
   build: {
     format: 'directory'
-  },
-  integrations: [
-    sitemap({
-      filter: (page) => !page.includes('/api/')
-    }),
-  ],
+  }
 });
